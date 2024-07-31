e4775ac98acc3c5be82dcea044edfd29454f7e43
Asmaa26189 <engasmaahesham@gmail.com>
2024-07-31 12:34:01 +0300

    23-7-2024

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
-"""N queens"""
+"""N queens solution finder module.
+"""
 import sys
 
 
 solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
 n = 0
+"""The size of the chessboard.
+"""
 pos = None
+"""The list of possible positions on the chessboard.
+"""
 
 
 def get_input():
-    """get_input"""
+    """Retrieves and validates this program's argument.
+
+    Returns:
+        int: The size of the chessboard.
+    """
     global n
     n = 0
     if len(sys.argv) != 2:
@@ -27,14 +38,29 @@ def get_input():
 
 
 def is_attacking(pos0, pos1):
-    """is_attacking"""
+    """Checks if the positions of two queens are in an attacking mode.
+
+    Args:
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
+
+    Returns:
+        bool: True if the queens are in an attacking position else False.
+    """
     if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
         return True
     return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
 
 
 def group_exists(group):
-    """group_exists"""
+    """Checks if a group exists in the list of solutions.
+
+    Args:
+        group (list of integers): A group of possible positions.
+
+    Returns:
+        bool: True if it exists, otherwise False.
+    """
     global solutions
     for stn in solutions:
         i = 0
@@ -48,7 +74,12 @@ def group_exists(group):
 
 
 def build_solution(row, group):
-    """build_solution"""
+    """Builds a solution for the n queens problem.
+
+    Args:
+        row (int): The current row in the chessboard.
+        group (list of lists of integers): The group of valid positions.
+    """
     global solutions
     global n
     if row == n:
@@ -67,7 +98,8 @@ def build_solution(row, group):
 
 
 def get_solutions():
-    """get_solutions"""
+    """Gets the solutions for the given chessboard size.
+    """
     global pos, n
     pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
     a = 0
